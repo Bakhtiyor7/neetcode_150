c871bfb5ca1fa1033bd600579f2c1bc730967021
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2026-08-01 20:17:34 +0900

    feat: reformat

--- array/isAnagram.js
+++ array/isAnagram.js
@@ -1,30 +1,29 @@
 class Solution {
-    /**
-     * @param {string} s
-     * @param {string} t
-     * @return {boolean}
-     */
-    isAnagram(s, t) {
-        if (s.length !== t.length) {
-            return false
-        }
-
-        let sArr = s.split("");
-        let tArr = t.split("");
-
-        sArr.sort()
-        tArr.sort()
-
-        for (let i = 0; i < sArr.length; i++) {
-            if (sArr[i] !== tArr[i]) {
-                return false
-            }
-        }
-        return true
-
-        // if same return true, else false
-        
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {
+    if (s.length !== t.length) {
+      return false;
     }
+
+    let sArr = s.split("");
+    let tArr = t.split("");
+
+    sArr.sort();
+    tArr.sort();
+
+    for (let i = 0; i < sArr.length; i++) {
+      if (sArr[i] !== tArr[i]) {
+        return false;
+      }
+    }
+    return true;
+
+    // if same return true, else false
+  }
 }
 
 const solution = new Solution();
